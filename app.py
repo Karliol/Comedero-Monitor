8bdb4f1c3741a8127139f1d7ebe36eee98d61a54
Karliol <139512959+Karliol@users.noreply.github.com>
2026-09-04 22:09:36 -0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+
 import os
 from datetime import datetime, timedelta
 from io import BytesIO
@@ -1180,3 +1181,4 @@ inicializar_plataforma()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=False)
+
